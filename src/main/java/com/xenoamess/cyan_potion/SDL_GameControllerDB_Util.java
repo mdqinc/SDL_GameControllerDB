6d3effedf272717afa3deab134c5211b95966152
XenoAmess <xenoamess@gmail.com>
2019-05-01 01:07:47 +0800

    ++version.

--- src/main/java/com/xenoamess/cyan_potion/SDL_GameControllerDB_Util.java
+++ src/main/java/com/xenoamess/cyan_potion/SDL_GameControllerDB_Util.java
@@ -5,6 +5,9 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
 
+/**
+ * @author XenoAmess
+ */
 public class SDL_GameControllerDB_Util {
     private static String SDL_GameControllerDB = null;
 
