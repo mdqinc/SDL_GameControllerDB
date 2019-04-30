bcc6c183ce494ab4240164606a7c2177d1cb1eb9
XenoAmess <xenoamess@gmail.com>
2019-04-30 15:58:28 +0800

    make it a wrapper

---
+++ src/main/java/com/xenoamess/cyan_potion/SDL_GameControllerDB_Util.java
@@ -0,0 +1,44 @@
+package com.xenoamess.cyan_potion;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+
+public class SDL_GameControllerDB_Util {
+    private static String SDL_GameControllerDB = null;
+
+    public static String getSDL_GameControllerDB() {
+        if (SDL_GameControllerDB == null) {
+            SDL_GameControllerDB = loadFile("/gamecontrollerdb.txt");
+        }
+        return SDL_GameControllerDB;
+    }
+
+    private static URL getURL(String resourceFilePath) {
+        final URL res = SDL_GameControllerDB_Util.class.getResource(resourceFilePath);
+        return res;
+    }
+
+    private static String loadFile(String resourceFilePath) {
+        String res = "";
+        try (
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(getURL(resourceFilePath).openStream()));
+        ) {
+            final StringBuffer sb = new StringBuffer();
+            String tmp;
+            while (true) {
+                tmp = bufferedReader.readLine();
+                if (tmp == null) {
+                    break;
+                }
+                sb.append(tmp);
+                sb.append("\n");
+            }
+            res = sb.toString();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return res;
+    }
+}
---
+++ src/test/java/Test.java
@@ -0,0 +1,7 @@
+import com.xenoamess.cyan_potion.SDL_GameControllerDB_Util;
+
+public class Test {
+    public static void main(String args[]) {
+        System.out.println(SDL_GameControllerDB_Util.getSDL_GameControllerDB());
+    }
+}
