c5992e0edc141be1fd0764c810792b4ead26e25c
Gabriel Jacobo <gabomdq@gmail.com>
2020-02-22 14:54:27 -0300

    Use proper english

--- check.py
+++ check.py
@@ -1,4 +1,3 @@
 #!/usr/bin/env python
 
-print ("check.py temporarily removed due to DMCA claim...it used tabs instead of spaces so nothing is good is lost")
-
+print ("check.py temporarily removed due to DMCA claim...it used tabs instead of spaces so nothing good is lost")
